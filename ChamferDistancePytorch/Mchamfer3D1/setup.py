a94a5034b41f6ade4cbbabb2c488674776ae6953
HSH <62924398+TransferHee@users.noreply.github.com>
2024-07-29 17:58:28 +0900

    Add files via upload

---
+++ ChamferDistancePytorch/Mchamfer3D1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='mchamfer_3D1',
+    ext_modules=[
+        CUDAExtension('mchamfer_3D1', [
+            "/".join(__file__.split('/')[:-1] + ['chamfer_cuda.cpp']),
+            "/".join(__file__.split('/')[:-1] + ['chamferM3D.cu']),
+        ]),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
